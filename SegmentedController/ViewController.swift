f669496f9aabbd64d8755a77d240ce1b4050d13d
Lina Isabel <isaaristi@unicauca.edu.co>
2017-05-20 09:54:05 -0500

    Se implemento la funcion de control de segmentos en viewController

--- SegmentedController/ViewController.swift
+++ SegmentedController/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var container: UIView!
+    var pantalla1:UIViewController!
+    var pantalla2:UIViewController!
+    var prev:UIViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        pantalla1 = storyboard!.instantiateViewController(withIdentifier: "pantalla1")
+        pantalla2 = storyboard!.instantiateViewController(withIdentifier: "pantalla2")
+        prev = pantalla1
+        addController(content: pantalla1)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func selectContent(_ sender: UISegmentedControl) {
+        let pos = sender.selectedSegmentIndex
+        removeController(content: prev)
+        if pos == 0{
+            addController(content: pantalla1)
+            prev = pantalla1
+        }else{
+            addController(content: pantalla2)
+            prev = pantalla2
+        }
+    }
+    
+    func addController(content:UIViewController){
+        addChildViewController(content)
+        content.view.frame.size = container.frame.size
+        content.view.frame.origin = CGPoint(x: 0, y: 0)
+        container.addSubview(content.view)
+        content.didMove(toParentViewController: self)
+    }
+    
+    func removeController(content:UIViewController){
+        content.willMove(toParentViewController: nil)
+        content.view.removeFromSuperview()
+        content.removeFromParentViewController()
+    }
 }
 
